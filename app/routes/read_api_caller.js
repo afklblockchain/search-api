59741729853f329c126b5570178176627502c2b2
Deniz Vahaboglu <deniz.vahaboglu@klm.com>
2018-10-25 12:21:29 +0200

    switched parameter fix

--- app/routes/read_api_caller.js
+++ app/routes/read_api_caller.js
@@ -1,34 +1,41 @@
-const request = require('request');
-const config = require('./config');
+const request = require('request')
+const config = require('./config')
 
 module.exports = {
-    getAirlines: async function () {
-        return new Promise(function (resolve, reject) {
-            request.get(`http://${config.app.host}:${config.app.port}/airlines`, (error, response, body) => {
-            
-                if(error) {
-                    reject(error);
-                }
-                
-                resolve(JSON.parse(body));
-            });
-        });
-    },
+  getAirlines: async function() {
+    return new Promise(function(resolve, reject) {
+      request.get(
+        `http://${config.app.host}:${config.app.port}/airlines`,
+        (error, response, body) => {
+          if (error) {
+            reject(error)
+          }
 
-    getOffers: async function(from, to, date, address) {
-        return new Promise(function (resolve, reject) {
-            request.get(`http://${config.app.host}:${config.app.port}/airlines/` +
-                 address + '/flight-offers?destination=' +
-                 from + '&origin=' +
-                 to + '&date=' +
-                 date, (error, response, body) => {
-            
-                if(error) {
-                    reject(error);
-                }
-                
-                resolve(JSON.parse(body));
-            });
-        });
-    }
+          resolve(JSON.parse(body))
+        }
+      )
+    })
+  },
+
+  getOffers: async function(from, to, date, address) {
+    return new Promise(function(resolve, reject) {
+      request.get(
+        `http://${config.app.host}:${config.app.port}/airlines/` +
+          address +
+          '/flight-offers?destination=' +
+          to +
+          '&origin=' +
+          from +
+          '&date=' +
+          date,
+        (error, response, body) => {
+          if (error) {
+            reject(error)
+          }
+
+          resolve(JSON.parse(body))
+        }
+      )
+    })
+  }
 }
